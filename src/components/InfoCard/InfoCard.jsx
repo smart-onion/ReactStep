f4d6e049ea3c3fff079136a1ef785d3149af773d
smart-onion <154732603+smart-onion@users.noreply.github.com>
2025-06-21 12:38:49 +0300

    React Inro

---
+++ src/components/InfoCard/InfoCard.jsx
@@ -0,0 +1,12 @@
+import "./InfoCard.css"
+
+export function InfoCard() {
+    return (
+        <div className="info-card">
+            <img src="https://www.englishfreeonline.com/wp-content/uploads/2022/11/My-Face-Vocabulary-English-Free-Online-Grammar-Lessons-And-Topics.jpg" alt="image"/>
+            <h3>Oleksandr</h3>
+            <p>+10000001</p>
+            <a href="#">oleksandr@gmail.com</a>
+        </div>
+    );
+}
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+import "./Title.css";
+
+export function Title(){
+    return(
+        <>
+            <h1 className="title">Welcome to My Website</h1>
+        </>
+    )
+}
